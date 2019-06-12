07c28f42377645a3c4c759ba2717439e764937da
tallan groberg <tallan.taven@gmail.com>
2019-06-12 15:24:52 -0600

    initialize

---
+++ app/controllers/api/items_controller.rb
@@ -0,0 +1,41 @@
+class Api::ItemsController < ApplicationController
+  before_action :set_department
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index
+    render json: @department.items
+  end
+
+  def show
+  end
+
+  def create
+    item = @department.items.new(item_params)
+    if @item.save
+      render json: @department.item
+    else
+      render json: item.errors status: 422 
+
+    end
+
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+  private
+
+    def set_department
+      @department = Department.find(params[:department_id])
+    end
+    def set_item
+      @item = Item.find(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :price)
+    end
+end
